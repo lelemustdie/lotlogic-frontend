2aa250df311a9a592410a9bb303cfaa0d1478028
nicom <nicolas.mangini@ing.austral.edu.ar>
2023-07-13 02:26:17 -0300

    add address of parking to reservation

--- src/components/Table/VehicleRegistryTable.js
+++ src/components/Table/VehicleRegistryTable.js
@@ -80,7 +80,7 @@ export const VehicleRegistryTable = ({rows,deleteRow,adminColumn = false,}) => {
                   <td>{row.vehicleModel}</td>
                   <td>{row.entryDate}</td>
                   <td>{row.exitDate}</td>
-                  {adminColumn && <td>{row.id}</td>}
+                  {adminColumn && <td>{row.parkingReservationAddress}</td>}
                   <td></td>
                 </tr>
               );
